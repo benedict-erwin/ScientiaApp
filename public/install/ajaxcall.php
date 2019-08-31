e302665efd69c6149bd9da2f507f5c43938900c9
Benedict E. Pranata <benedict.erwin@gmail.com>
2019-08-31 11:54:59 +0700

    Update ajaxcall.php
    Disable error reporting

--- public/install/ajaxcall.php
+++ public/install/ajaxcall.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(0);
 session_start();
 
 require_once '../../src/ScientiaAPP/App/Lib/Encrypter.php';
